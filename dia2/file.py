609444f17fbe3f10a195f5331f1c79e943006d4f
PedroSehn <pedrorsehn@hotmail.com>
2023-02-03 23:32:54 -0300

    primeiro comit

--- dia2/file.py
+++ dia2/file.py
@@ -1,16 +1,16 @@
 
 try:
-  text_file = open('dia2/teste.txt', mode="w")
+    text_file = open('dia2/teste.txt', mode="w")
 except:
-  print('Arquivo nao existe')
-else:  
-  list = ['uno', 'duno']
-  text_file.write('TESTANO')
-  text_file.write('TESTANO2')
+    print('Arquivo nao existe')
+else:
+    list = ['uno', 'duno']
+    text_file.write('TESTANO')
+    text_file.write('TESTANO2')
 
-  text_file = open('dia2/teste.txt', mode="r")
-  content = text_file.read()
-  print(content)
+    text_file = open('dia2/teste.txt', mode="r")
+    content = text_file.read()
+    print(content)
 finally:
     print('tentando abrir o arquivo')
 
